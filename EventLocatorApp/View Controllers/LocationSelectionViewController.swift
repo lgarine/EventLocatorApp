f26d1fa4e4daf6c5290d8e4e87b941e82272cf6a
lgarine <prasannagarine@gmail.com>
2019-01-24 18:44:17 -0600

    Location Selection dropdown

--- EventLocatorApp/View Controllers/LocationSelectionViewController.swift
+++ EventLocatorApp/View Controllers/LocationSelectionViewController.swift
@@ -12,11 +12,22 @@ protocol LocationSelectionViewControllerDelegate: class {
     func LocationSelectionVCIsDone(city:String?)
 }
 
-class LocationSelectionViewController: UIViewController {
+class LocationSelectionViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
 
+    @IBOutlet weak var tableView: UITableView!
     
     weak var delegate: LocationSelectionViewControllerDelegate?
       var rightBarButton:UIBarButtonItem?
+    
+    let statusBarHeight = UIApplication.shared.statusBarFrame.height
+    let sectionArray = ["Country", "State", "City"]
+    let cellArray = [
+        ["USA", "India", "Australia"],
+        ["Kansas", "SFO", "Arizona", "Nebraska"],
+        ["Overland Park", "Freemont", "Omaha", "Lees Summit", "Lenexa"]
+    ]
+    var isShowing = [false, false, false]
+    var selectedIndex = [0, 0, 0]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,19 +44,70 @@ class LocationSelectionViewController: UIViewController {
     @objc func doneButtonAction()
     {
         
-//    delegate?.LocationSelectionVCIsDone(city: "Kansas")
+//   delegate?.LocationSelectionVCIsDone(city: "Kansas")
         self.navigationController?.dismiss(animated: true, completion: nil)
         
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        //return self.sectionArray.count
+        return self.sectionArray.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if !self.isShowing[section] {
+            return 1;
+        } else {
+            return self.cellArray[section].count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.sectionArray[section]
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let id = "cell"
+        var cell = tableView.dequeueReusableCell(withIdentifier: id)
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: id)
+        }
+        
+        if !self.isShowing[indexPath.section] {
+            cell?.textLabel?.text = self.cellArray[indexPath.section][self.selectedIndex[indexPath.section]]
+            cell?.accessoryType = .disclosureIndicator
+        } else {
+            cell?.textLabel?.text = self.cellArray[indexPath.section][indexPath.row]
+            if indexPath.row == self.selectedIndex[indexPath.section] {
+                cell?.accessoryType = .checkmark
+            } else {
+                cell?.accessoryType = .none
+            }
+        }
+        
+        return cell!
+    }
+    
+    
+    // MARK: - UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //tableView.deselectRow(at: indexPath, animated: true)
+        if self.isShowing[indexPath.section] {
+            self.selectedIndex[indexPath.section] = indexPath.row
+            
+            let arr1 = self.cellArray[2]
+            
+             delegate?.LocationSelectionVCIsDone(city: arr1[indexPath.row] )
+            
+        }
+        self.isShowing[indexPath.section] = !self.isShowing[indexPath.section]
+        tableView.reloadSections(IndexSet(integer: indexPath.section), with: .fade)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 48
+    }
+    
+    func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+    }
 }
